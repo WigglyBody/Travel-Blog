9a721952ab97f08d1e55f711ec6d8015f48e0a82
Emilio Moya R <emilioem2007@gmail.com>
2024-11-10 14:16:04 -0500

    Js de Landing Page

---
+++ assets/js/landing.js
@@ -0,0 +1,23 @@
+// Filtro de blogs
+$(document).ready(function(){
+    $(".filter-item").click(function(){
+        const value = $(this).attr("data-filter");
+        if(value == "all"){
+            $(".post-box").show("1000");
+        }
+        else{
+            $(".post-box").not("."+value).hide("1000");
+            $(".post-box").filter("."+value).show("1000");
+        }
+    });
+    //Activación por botón
+    $(".filter-item").click(function(){
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+    });
+});
+//Cambio de color de Background del Header al bajar
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+    header.classList.toogle("shadow", window.scrollY > 0);
+});
