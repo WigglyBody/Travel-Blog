eb18f0578ef67d049a673ceb93950766c7c03bdb
dkac14 <150403101+dkac14@users.noreply.github.com>
2024-11-12 18:19:29 -0500

    landing.js

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,27 +1,21 @@
-// Filtro de blogs
-$(document).ready(function(){
-    $(".filter-item").click(function(){
-        const value = $(this).attr("data-filter");
-        if(value == "all"){
-            $(".post-box").show("1000");
-        }
-        else{
-            $(".post-box")
-            .not("."+value)
-            .hide("1000");
-            $(".post-box")
-            .filter("."+value)
-            .show("1000");
+$(document).ready(function() {
+    $('.filter-item').click(function() {
+        const value = $(this).attr('data-filter');
+        if (value === 'all') {
+            $('.post-box').show('1000');
+        } else {
+            $('.post-box').not('.' + value).hide('1000');
+            $('.post-box').filter('.' + value).show('1000');
         }
     });
-    //Activación por botón
-    $(".filter-item").click(function(){
-    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+
+    $('.filter-item').click(function() {
+        $(this).addClass("active-filter").siblings().removeClass("active-filter");
     });
 });
-//Cambio de color de Background del Header al bajar
-let header = document.querySelector("header");
 
-window.addEventListener("scroll", () => {
-    header.classList.toggle("shadow", window.scrollY > 0);
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
 });
