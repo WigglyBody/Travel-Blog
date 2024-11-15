6e6932a384de22f1b47d6d758cce3d73d628a62b
Emilio Moya R <emilioem2007@gmail.com>
2024-11-14 23:58:08 -0500

    Se dió formato a los documentos para mejor visualización, creación de páginas de blogs top.

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,33 +1,43 @@
-$(document).ready(function() {
-    // Mostrar publicaciones cuando se carga la página
-    loadPosts();
+$(document).ready(function () {
+  // Mostrar publicaciones cuando se carga la página
+  loadPosts();
 
-    $('.filter-item').click(function() {
-        const value = $(this).attr('data-filter');
-        if (value === 'all') {
-            $('.post-box').show('1000');
-        } else {
-            $('.post-box').not('.' + value).hide('1000');
-            $('.post-box').filter('.' + value).show('1000');
-        }
-    });
-
-    $('.filter-item').click(function() {
-        $(this).addClass("active-filter").siblings().removeClass("active-filter");
-    });
-    
-    // Función para cargar las publicaciones desde localStorage
-    function loadPosts() {
-        const posts = JSON.parse(localStorage.getItem('posts')) || [];
-        posts.forEach(post => {
-            const postBox = createPostBox(post.title, post.category, post.description, post.date, post.author);
-            $('.post').append(postBox);
-        });
+  $(".filter-item").click(function () {
+    const value = $(this).attr("data-filter");
+    if (value === "all") {
+      $(".post-box").show("1000");
+    } else {
+      $(".post-box")
+        .not("." + value)
+        .hide("1000");
+      $(".post-box")
+        .filter("." + value)
+        .show("1000");
     }
+  });
 
-    // Función para crear un bloque de publicación
-    function createPostBox(title, category, description, date, author) {
-        return `
+  $(".filter-item").click(function () {
+    $(this).addClass("active-filter").siblings().removeClass("active-filter");
+  });
+
+  // Función para cargar las publicaciones desde localStorage
+  function loadPosts() {
+    const posts = JSON.parse(localStorage.getItem("posts")) || [];
+    posts.forEach((post) => {
+      const postBox = createPostBox(
+        post.title,
+        post.category,
+        post.description,
+        post.date,
+        post.author
+      );
+      $(".post").append(postBox);
+    });
+  }
+
+  // Función para crear un bloque de publicación
+  function createPostBox(title, category, description, date, author) {
+    return `
             <div class="post-box ${category.toLowerCase()}">
                 <h2 class="category">${category}</h2>
                 <a href="#" class="post-title">${title}</a>
@@ -38,11 +48,11 @@ $(document).ready(function() {
                 </div>
             </div>
         `;
-    }
+  }
 });
 
-let header = document.querySelector('header');
+let header = document.querySelector("header");
 
-window.addEventListener('scroll', () => {
-    header.classList.toggle('shadow', window.scrollY > 0);
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
 });
